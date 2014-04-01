60bb6221d04e73b0429337d880bf8dd3ba42ef99
Subhash Chandran <subwiz@gmail.com>
2014-04-01 11:52:29 +0530

    unused import removed.

--- src/main/java/org/wiztools/stagen/STTemplateExecutor.java
+++ src/main/java/org/wiztools/stagen/STTemplateExecutor.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.util.Map;
 import org.stringtemplate.v4.ST;
-import org.stringtemplate.v4.STGroupDir;
+import org.stringtemplate.v4.STGroup;
 import org.stringtemplate.v4.STRawGroupDir;
 import org.wiztools.commons.Charsets;
 
@@ -23,7 +23,7 @@ public class STTemplateExecutor implements TemplateExecutor {
             final File tmplDir = templateFile.getParentFile();
             final String tmplName = Util.getBaseFileName(templateFile.getName());
             
-            STGroupDir stg = new STRawGroupDir(tmplDir.toURI().toURL(),
+            STGroup stg = new STRawGroupDir(tmplDir.toURI().toURL(),
                     Charsets.UTF_8.name(),
                     delimiterStartChar,
                     delimiterStopChar);
